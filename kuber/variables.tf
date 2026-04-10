d35d836fb9a87b3f83cae03df16c567786e44fc2
sem <krasnov_sem@mail.ru>
2026-04-10 18:45:31 +0300

    Terraform 3 ВМ для Proxmox

---
+++ kuber/variables.tf
@@ -0,0 +1,60 @@
+variable "vm_cores" {
+  default = 2
+}
+
+variable "vm_memory" {
+  default = 4096
+}
+
+variable "vm_disk" {
+  type = string
+  default = "40G"
+}
+
+variable "vm_count" {
+  default = 3
+}
+
+variable "vm_name_prefix" {
+  type = string
+  default = "kuber"
+}
+
+variable "vm_description" {
+  type = string 
+  default = "Kubernetes cluster virtual machine"
+}
+
+variable "vm_id" {
+  default = 500
+}
+
+variable "ci_user" {
+  type = string
+  sensitive = true
+}
+
+variable "ci_pass" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+  sensitive = true
+}
